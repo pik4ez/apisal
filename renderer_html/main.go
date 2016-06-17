0f02229e74a245debc8259ce10ff22f55b995d9b
Dmitry Khasanov <dkhasanov@avito.ru>
2016-06-18 01:55:29 +0300

    Html renderer stage 1 complete.

--- renderer_html/main.go
+++ renderer_html/main.go
@@ -1,21 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
-	"fmt"
 	"html/template"
+	"io"
+	"log"
 	"os"
 
 	"github.com/pik4ez/apisal/apisal"
 )
 
 func main() {
-	var inputObjOne = []byte("{\"point\": {\"lat\": 10.0, \"lng\": 10.0, \"time\": \"2016-06-17T20:30:25+0000\"}, \"title\": \"obj_one_title\", \"description\": \"obj_one_description\", \"images\": [{\"w\": 100, \"h\": 50, \"title\": \"img_one_title\", \"url\": \"http://example.org/image.png\"}]}")
-	var objs []apisal.Object
-	var objOne apisal.Object
-	json.Unmarshal(inputObjOne, &objOne)
-	objs = append(objs, objOne)
-	fmt.Println(objOne.Images[0])
+	if os.Stdin == nil {
+		log.Fatal("You should provide objects.")
+	}
+	objs, err := ReadObjects(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
 	t := template.Must(template.ParseFiles("templates/simple.html"))
 	t.Execute(os.Stdout, objs)
+}
+
+// ReadObjects reads all objects from stdin.
+func ReadObjects(r io.Reader) ([]apisal.Object, error) {
+	var objects []apisal.Object
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		object := apisal.Object{}
+		err := json.Unmarshal([]byte(scanner.Text()), &object)
+		if err != nil {
+			return nil, err
+		}
+		objects = append(objects, object)
+	}
+	return objects, nil
 }
