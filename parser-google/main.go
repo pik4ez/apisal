fbd41f9d6390ed381b856bc232197d5b64d5586d
Qinabu <qinabu@gmail.com>
2016-06-18 01:31:57 +0300

    AllPoints

--- parser-google/main.go
+++ parser-google/main.go
@@ -7,23 +7,53 @@ import (
 	"time"
 	"encoding/json"
 	"log"
+	"io"
 )
 
 type Point struct {
-	Lat float64 `json:"lat"`
-	Lon float64 `json:"lon"`
+	Lat  float64 `json:"lat"`
+	Lon  float64 `json:"lon"`
 	Time time.Time `json:"time"`
 }
 
+type Object struct {
+}
+
+type WikiObject struct {
+	Title string `json:"title"`
+}
+
 func main() {
+	if os.Stdin == nil {
+		log.Fatal("sdfsdf")
+	}
+	points, err := AllPoints(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, point := range points {
+		fmt.Println(point.Lat, point.Lon, point.Time)
+	}
+}
+
+func AllPoints(r io.Reader) ([]Point, error) {
+	var points []Point
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		fmt.Println(scanner.Text())
 		point := Point{}
 		err := json.Unmarshal([]byte(scanner.Text()), &point)
 		if err != nil {
-			log.Fatal(err)
+			return nil, err
 		}
-		fmt.Println(point.Lat, point.Lon, point.Time)
+		points = append(points, point)
 	}
+
+	return points, nil
 }
+
+//func Objects(point Point) ([]Object, error) {
+//
+//
+//}
